b230bbb695e6d922cf52fc322257acb377d042c9
b3nj4m1n <b3nj4m1n@gmx.net>
2021-09-23 21:48:14 +0200

    Add search & remove methods Also added a method for printing the tree in dot language

--- thestuff/binary_search_tree/src/BinarySearchTree.h
+++ thestuff/binary_search_tree/src/BinarySearchTree.h
@@ -28,6 +28,7 @@ class BinarySearchTree {
     private:
     BinarySearchTree<T>* subtree_left;
     BinarySearchTree<T>* subtree_right;
+    BinarySearchTree<T>* parent;
     T* content;
 
     public:
@@ -36,12 +37,20 @@ class BinarySearchTree {
     BinarySearchTree(T content);
     bool is_empty();
     bool is_leaf();
+    bool has_only_one_child();
     void set_content(T* content);
+    T* get_content();
+    void set_parent(BinarySearchTree<T>* parent);
+    void overwrite_self_parent(BinarySearchTree<T>* new_subtree);
     void insert(T* content);
+    void insert(T* content, BinarySearchTree<T>* caller);
+    BinarySearchTree<T>* search(T* content);
+    T* remove();
     void print();
     void print_pre_order();
     void print_in_order();
     void print_post_order();
+    void print_dot(int* count, int parent_id);
 };
 
 template <class T>
@@ -77,6 +86,15 @@ bool BinarySearchTree<T>::is_leaf() {
     return true;
 }
 
+// Will return true if the subtree only has one other subtree
+template <class T>
+bool BinarySearchTree<T>::has_only_one_child() {
+    if (this->is_empty()) { return false; }
+    if (!this->subtree_left->is_empty() && this->subtree_right->is_empty()) { return true; }
+    if (this->subtree_left->is_empty() && !this->subtree_right->is_empty()) { return true; }
+    return false;
+}
+
 // Sets content, creates emtpy subtrees if subtrees don't exist yet
 template <class T>
 void BinarySearchTree<T>::set_content(T* content) {
@@ -85,13 +103,88 @@ void BinarySearchTree<T>::set_content(T* content) {
     if (this->subtree_right == nullptr) { this->subtree_right = new BinarySearchTree<T>; }
 }
 
-// Inserts content at appropriate position, if content already exists, nothing happens
+// Return pointer to content
+template <class T>
+T* BinarySearchTree<T>::get_content() {
+    return this->content;
+}
+
+// Return pointer to content
+template <class T>
+void BinarySearchTree<T>::set_parent(BinarySearchTree<T>* parent) {
+    this->parent = parent;
+}
+
+// Find itself in the parent, then overwrite that subtree
+template <class T>
+void BinarySearchTree<T>::overwrite_self_parent(BinarySearchTree<T>* new_subtree) {
+    if (this->parent->subtree_left == this) {
+        this->parent->subtree_left = new_subtree;
+    }
+    if (this->parent->subtree_right == this) {
+        this->parent->subtree_right = new_subtree;
+    }
+}
+
 template <class T>
 void BinarySearchTree<T>::insert(T* content) {
-    if (this->is_empty()) { this->set_content(content); }
+    this->insert(content, nullptr);
+}
+// Inserts content at appropriate position, if content already exists, nothing happens
+template <class T>
+void BinarySearchTree<T>::insert(T* content, BinarySearchTree<T>* caller) {
+    if (this->is_empty()) { this->set_content(content); this->set_parent(caller); }
     if (this->content == content) { }
-    else if (*(this->content) > *content) { this->subtree_left->insert(content); }
-    else if (*(this->content) < *content) { this->subtree_right->insert(content); }
+    else if (*(this->content) > *content) { this->subtree_left->insert(content, this); }
+    else if (*(this->content) < *content) { this->subtree_right->insert(content, this); }
+}
+
+// Returns null if the content doesn't exist, otherwise returns the subtree containing the content
+template <class T>
+BinarySearchTree<T>* BinarySearchTree<T>::search(T* content) {
+    if (this->is_empty()) { return nullptr; }
+    if (*(this->content) == *(content)) { return this; }
+    if (*(this->content) > *(content)) { return this->subtree_left->search(content); }
+    if (*(this->content) < *(content)) { return this->subtree_right->search(content); }
+}
+
+// Removes the subtree, returns the value
+template <class T>
+T* BinarySearchTree<T>::remove() {
+    T* content = this->get_content();
+    // There are three cases that need to be handled differently:
+    // The subtree is a leaf (no subtrees are attached to it) - We can just delete it
+    if (this->is_leaf()) {
+        this->overwrite_self_parent(new BinarySearchTree<T>);
+    }
+    // The subtree only has one subtree attached to it - We can simply move the
+    // pointer of the parent to this child
+    else if (this->has_only_one_child()) {
+        if (!this->subtree_left->is_empty()) {
+            this->overwrite_self_parent(this->subtree_left);
+            this->subtree_left->set_parent(this->parent);
+        }
+        if (!this->subtree_right->is_empty()) {
+            this->overwrite_self_parent(this->subtree_right);
+            this->subtree_right->set_parent(this->parent);
+        }
+    }
+    // The subtree has two subtrees attached to it - Move to the right subtree,
+    // go left as many times as possible, copy this subtree to the position where
+    // we want to remove our subtree, then call delete on the replacement
+    else {
+        // Find the leftmost subtree of the right subtree
+        BinarySearchTree<T>* current = this->subtree_right;
+        while (!current->subtree_left->is_empty()) {
+            current = current->subtree_left;
+        }
+        // Replace the value of the subtree we're trying to remove with the value of
+        // the subtree we just found
+        this->set_content(current->get_content());
+        // Remove the subtree from the tree
+        current->remove();
+    }
+    return content;
 }
 
 // Prints general information about current subtree
@@ -108,6 +201,7 @@ void BinarySearchTree<T>::print() {
     } else {
         std::cout << "Right empty.";
     }
+    std::cout << "Parent: " << *(this->parent->content) << ", ";
     std::cout << std::endl;
 }
 
@@ -131,6 +225,29 @@ void BinarySearchTree<T>::print_in_order() {
     this->subtree_left->print_in_order();
     std::cout << *(this->content) << std::endl;
     this->subtree_right->print_in_order();
+}
+
+template <class T>
+void BinarySearchTree<T>::print_dot(int* counter, int parent_id) {
+    if (this->is_empty()) { return; }
+    (*counter)++;
+    int id = *counter;
+    if (parent_id == 0) {
+        std::cout << "digraph BST {" << std::endl;
+    }
+    std::cout << "\t" << "node" << *counter << "[label=\"" << *(this->content) << "\"];" << std::endl;
+    if (parent_id != 0) {
+        std::cout << "\t" << "node" << parent_id << " -> " << "node" << id << ";" << std::endl;
+    }
+    if (!this->subtree_left->is_empty()) {
+        this->subtree_left->print_dot(counter, id);
+    }
+    if (!this->subtree_right->is_empty()) {
+        this->subtree_right->print_dot(counter, id);
+    }
+    if (parent_id == 0) {
+        std::cout << "}" << std::endl;
+    }
 }
 
 #endif
--- thestuff/binary_search_tree/src/main.cpp
+++ thestuff/binary_search_tree/src/main.cpp
@@ -2,16 +2,33 @@
 #include "BinarySearchTree.h"
 
 int main(int argc, char** argv) {
-    char content = 'A';
-    int values[] = {44,17,32,78,88,50,48,62};
+    // int values[] = {44,17,32,78,88,50,48,62};
+    int values[] = {31, 6, 2, 19, 7, 11, 45, 28, 25, 41, 9, 4, 43, 10, 23, 34, 18, 42, 47, 38, 12, 35, 29, 16, 39, 30, 37, 50, 46, 33, 48, 40, 1, 15, 3, 5, 27, 22, 8, 20, 44, 13, 49, 21, 26, 24, 14, 32, 17, 36};
     BinarySearchTree<int>* tree = new BinarySearchTree<int>;
     for (int i = 0; i < (sizeof(values)/sizeof(*values)); i++) {
         tree->insert(&values[i]);
     }
     // tree->print();
-    tree->print_post_order();
+    // tree->print_post_order();
+
     /* std::cout << tree->is_empty() << std::endl;
     tree->insert(&content);
     std::cout << tree->is_empty() << std::endl; */
+
+    int content = 50;
+    BinarySearchTree<int>* result = tree->search(&content);
+    // std::cout << *(result->get_content()) << std::endl;
+
+    int values_to_remove[] = {14,35,19};
+    for (int i = 0; i < (sizeof(values_to_remove)/sizeof(*values_to_remove)); i++) {
+        BinarySearchTree<int>* subtree = tree->search(&values_to_remove[i]);
+        subtree->remove();
+        // subtree->print();
+        // std::cout << subtree->is_leaf() << std::endl;
+    }
+
+    int counter = 0;
+    tree->print_dot(&counter, 0);
+
     return 0;
 }
