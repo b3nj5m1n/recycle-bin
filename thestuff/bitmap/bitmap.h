3a5e547ddf74bc59590d3d655b6e77ba64577a7f
b3nj4m1n <b3nj4m1n@gmx.net>
2021-09-29 23:17:25 +0200

    Seems to be working, but can't change colors yet

--- thestuff/bitmap/bitmap.h
+++ thestuff/bitmap/bitmap.h
@@ -5,25 +5,7 @@
 #include <iostream>
 #include <stdint.h>
 #include <stdio.h>
-
-#ifndef SWAP_ENDIAN
-#define SWAP_ENDIAN
-template <typename T> T swap_endian(T u) {
-  static_assert(CHAR_BIT == 8, "CHAR_BIT != 8");
-
-  union {
-    T u;
-    unsigned char u8[sizeof(T)];
-  } source, dest;
-
-  source.u = u;
-
-  for (size_t k = 0; k < sizeof(T); k++)
-    dest.u8[k] = source.u8[sizeof(T) - k - 1];
-
-  return dest.u;
-}
-#endif
+#include <vector>
 
 #ifndef BITMAP_LIB
 #define BITMAP_LIB
@@ -31,6 +13,44 @@ template <typename T> T swap_endian(T u) {
 #define byte uint8_t
 #define t_bytes uint16_t
 #define f_bytes uint32_t
+
+template <class T> class Pixel {
+private:
+  T red;
+  T green;
+  T blue;
+
+public:
+  Pixel() {
+    this->red = 0;
+    this->green = 0;
+    this->blue = 0;
+  }
+  Pixel(T red, T green, T blue) {
+    this->red = red;
+    this->green = green;
+    this->blue = blue;
+  }
+};
+
+class PixelMap {
+private:
+  std::vector<Pixel<uint16_t>> pixels;
+  int width;
+  int height;
+  int row_size;
+
+public:
+  PixelMap() {}
+  PixelMap(int width, int height, int row_size) {
+    this->width = width;
+    this->height = height;
+    this->row_size = row_size;
+    this->pixels = std::vector<Pixel<uint16_t>>(this->height * this->row_size,
+                                                Pixel<uint16_t>());
+  }
+  std::vector<Pixel<uint16_t>> get_pixel_map() { return this->pixels; }
+};
 
 struct BITMAPFILEHEADER {
   t_bytes type; // Type of the file as ASCII text, standard is 42'4D ("BM")
@@ -69,78 +89,70 @@ private:
   int row_size;
   BITMAPFILEHEADER header_file;
   BITMAPINFOHEADER header_info;
+  PixelMap pixel_map;
 
 public:
-  Bitmap(int width, int height, int bits_per_pixel);
-  BITMAPFILEHEADER get_header_file();
-  BITMAPINFOHEADER get_header_info();
-  int write(std::string filename);
+  Bitmap(int width, int height, int bits_per_pixel) {
+    this->width = width;
+    this->height = height;
+    this->bits_per_pixel = bits_per_pixel;
+    this->row_size = ceil((this->bits_per_pixel * this->width) / 32) * 4;
+    this->header_file = this->get_header_file();
+    this->header_info = this->get_header_info();
+    this->pixel_map = PixelMap(this->width, this->height, this->row_size);
+  }
+
+  BITMAPFILEHEADER get_header_file() {
+    BITMAPFILEHEADER result;
+    result.type = 0x4D42;
+    result.size = 0x28 + (this->row_size * this->height);
+    result.res1 = 0x0;
+    result.res2 = 0x0;
+    result.offset = 0x36;
+    return result;
+  }
+  BITMAPINFOHEADER get_header_info() {
+    BITMAPINFOHEADER result;
+    result.struc_size = 0x28;
+    result.width = this->width;
+    result.height = this->height;
+    result.num_plans = 0x1;
+    result.num_bits_per_pixel = this->bits_per_pixel;
+    result.compression = 0x0;
+    result.num_bytes_img = (this->row_size * this->height);
+    result.resolution_hor = 0xC40E;
+    result.resolution_ver = 0xC40E;
+    result.num_colors = 0x0;
+    result.num_i_colors = 0x0;
+    return result;
+  }
+
+  int write(std::string filename) {
+    FILE *file = std::fopen(filename.c_str(), "wb");
+    // File Header
+    std::fwrite(&this->header_file.type, 2, 1, file);
+    std::fwrite(&this->header_file.size, 4, 1, file);
+    std::fwrite(&this->header_file.res1, 2, 1, file);
+    std::fwrite(&this->header_file.res2, 2, 1, file);
+    std::fwrite(&this->header_file.offset, 4, 1, file);
+    // Info Header
+    std::fwrite(&this->header_info.struc_size, 4, 1, file);
+    std::fwrite(&this->header_info.width, 4, 1, file);
+    std::fwrite(&this->header_info.height, 4, 1, file);
+    std::fwrite(&this->header_info.num_plans, 2, 1, file);
+    std::fwrite(&this->header_info.num_bits_per_pixel, 2, 1, file);
+    std::fwrite(&this->header_info.compression, 4, 1, file);
+    std::fwrite(&this->header_info.num_bytes_img, 4, 1, file);
+    std::fwrite(&this->header_info.resolution_hor, 4, 1, file);
+    std::fwrite(&this->header_info.resolution_ver, 4, 1, file);
+    std::fwrite(&this->header_info.num_colors, 4, 1, file);
+    std::fwrite(&this->header_info.num_i_colors, 4, 1, file);
+    for (auto i : this->pixel_map.get_pixel_map()) {
+      std::fwrite(&i, sizeof(i), 1, file);
+    }
+    fclose(file);
+    return 0;
+  }
 };
-
-Bitmap::Bitmap(int width, int height, int bits_per_pixel) {
-  this->width = width;
-  this->height = height;
-  this->bits_per_pixel = bits_per_pixel;
-  this->row_size = ceil((this->bits_per_pixel * this->width) / 32) * 4;
-  this->header_file = this->get_header_file();
-  this->header_info = this->get_header_info();
-}
-
-BITMAPFILEHEADER Bitmap::get_header_file() {
-  BITMAPFILEHEADER result;
-  result.type = 0x424D;
-  result.size = 0x28 + (this->row_size * this->height);
-  result.res1 = 0x0000;
-  result.res2 = 0x0000;
-  result.offset = 0x36000000;
-  return result;
-}
-BITMAPINFOHEADER Bitmap::get_header_info() {
-  BITMAPINFOHEADER result;
-  result.struc_size = 0x28000000;
-  result.width = this->width;
-  result.height = this->height;
-  result.num_plans = 0x1000;
-  result.num_bits_per_pixel = this->bits_per_pixel;
-  result.compression = 0x00000000;
-  result.num_bytes_img = (this->row_size * this->height);
-  result.resolution_hor = 0xC40E0000;
-  result.resolution_ver = 0xC40E0000;
-  result.num_colors = 0x00000000;
-  result.num_i_colors = 0x00000000;
-  return result;
-}
-
-template <typename T>
-void fwrite_endian(T *ptr, size_t size, size_t count, FILE *stream) {
-  // std::cout << std::hex << *ptr << std::endl;
-  T swapped = swap_endian<T>(*ptr);
-  std::fwrite(&swapped, size, count, stream);
-}
-
-int Bitmap::write(std::string filename) {
-  FILE *file = std::fopen(filename.c_str(), "wb");
-    std::cout << this->header_file.size << std::endl;
-  // File Header
-  fwrite_endian<t_bytes>(&this->header_file.type, 2, 1, file);
-  fwrite_endian<f_bytes>(&this->header_file.size, 4, 1, file);
-  fwrite_endian<t_bytes>(&this->header_file.res1, 2, 1, file);
-  fwrite_endian<t_bytes>(&this->header_file.res2, 2, 1, file);
-  fwrite_endian<f_bytes>(&this->header_file.offset, 4, 1, file);
-  // Info Header
-  fwrite_endian<f_bytes>(&this->header_info.struc_size, 4, 1, file);
-  fwrite_endian<f_bytes>(&this->header_info.width, 4, 1, file);
-  fwrite_endian<f_bytes>(&this->header_info.height, 4, 1, file);
-  fwrite_endian<t_bytes>(&this->header_info.num_plans, 2, 1, file);
-  fwrite_endian<t_bytes>(&this->header_info.num_bits_per_pixel, 2, 1, file);
-  fwrite_endian<f_bytes>(&this->header_info.compression, 4, 1, file);
-  fwrite_endian<f_bytes>(&this->header_info.num_bytes_img, 4, 1, file);
-  fwrite_endian<f_bytes>(&this->header_info.resolution_hor, 4, 1, file);
-  fwrite_endian<f_bytes>(&this->header_info.resolution_ver, 4, 1, file);
-  fwrite_endian<f_bytes>(&this->header_info.num_colors, 4, 1, file);
-  fwrite_endian<f_bytes>(&this->header_info.num_i_colors, 4, 1, file);
-  fclose(file);
-  return 0;
-}
 
 #endif
