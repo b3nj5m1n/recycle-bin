b8dfc0d29dfdd851a6d13911af3ce7a7dce06e97
b3nj4m1n <b3nj4m1n@gmx.net>
2021-10-04 14:47:22 +0200

    Holy shit it actually works

--- thestuff/tiny_renderer/src/main.cpp
+++ thestuff/tiny_renderer/src/main.cpp
@@ -5,6 +5,13 @@
 int main(int argc, char **argv) {
   Renderer renderer(500, 500, "./bin/test_render.bm");
   Model model("./assets/african_head.obj");
-  renderer.draw_wireframe(model, Color(0, 0, 255));
+  // Vertex v = model.get_vertex_at(1);
+  // std::cout << v << std::endl;
+  /* Face face = model.get_faces()[0];
+  Vertex v0 = model.get_vertex_at(face.l1.vertex);
+  Vertex v1 = model.get_vertex_at(face.l2.vertex); */
+  // renderer.draw_wireframe(model, Color(0, 0, 255));
+  // renderer.draw_face(face, model, Color(255, 0, 0));
+  renderer.draw_wireframe(model, Color(255, 0, 102));
   renderer.render();
 }
