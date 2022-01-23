855efb6d90e310c2cf44e6b6d3dde7aca1ef942e
b3nj4m1n <b3nj4m1n@gmx.net>
2022-01-23 20:49:11 +0100

    Add stats, properly overwrite prompt

--- thestuff/memo_trainer/memo.py
+++ thestuff/memo_trainer/memo.py
@@ -1,8 +1,11 @@
-import argparse
-import os
-import string
-import random
 from abc import ABC, abstractmethod
+from datetime import datetime
+from prettytable import PrettyTable
+import argparse
+import humanize
+import os
+import random
+import string
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-m", "--mode", required=True, type=str)
@@ -19,9 +22,13 @@ class Mode(ABC):
         self.i = 0
         self.rnd = random.Random(seed)
         self.current = self.rnd.random()
+        self.time_memo_started = datetime.now()
+        self.time_memo_finished = None
+        self.time_answer_checked = None
     def new(self):
         self.i += 1
         self.current = self.rnd.random()
+        self.time_generated = datetime.now()
         pass
     @abstractmethod
     def get_prompt(self):
@@ -31,7 +38,19 @@ class Mode(ABC):
         pass
     @abstractmethod
     def check_answer(self, answer):
+        self.time_answer_checked = datetime.now()
         pass
+    def finish_memo(self):
+        self.time_memo_finished = datetime.now()
+    def stats(self):
+        pt = PrettyTable()
+        pt.field_names = ["Time Memo", "Time Recall", "Time Total"]
+        pt.add_row([
+            humanize.precisedelta(self.time_memo_finished - self.time_memo_started, minimum_unit="seconds"),
+            humanize.precisedelta(self.time_answer_checked - self.time_memo_finished, minimum_unit="seconds"),
+            humanize.precisedelta(self.time_answer_checked - self.time_memo_started, minimum_unit="seconds")
+        ])
+        return pt.get_string()
 
 class ModeLetters(Mode):
     def __init__(self, *args):
@@ -45,6 +64,7 @@ class ModeLetters(Mode):
     def get_answer(self):
         return self.get_prompt()
     def check_answer(self, answer):
+        super().check_answer(self)
         table = answer.maketrans('', '', ' \n\t\r')
         answer = answer.strip().lower().translate(table)
         correct = self.get_answer().strip().lower().translate(table)
@@ -70,8 +90,12 @@ while True:
     generator.new()
     print(generator.get_prompt(), end='\r')
     os.system('read -s -n 1')
-    answer = input("Enter your answer:\n> ")
+    generator.finish_memo()
+    prompt = "Enter your answer"
+    space = ''.join([" " for _ in  range(len(generator.get_answer()) - len("Enter your answer:"))])
+    answer = input(f"{prompt}:{space}\n> ")
     if generator.check_answer(answer):
         print("Correct")
     else:
         print(f"The correct answer would have been: {generator.get_answer()}")
+    print(generator.stats())
