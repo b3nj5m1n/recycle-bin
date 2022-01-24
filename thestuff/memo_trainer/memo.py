c91e39ba950056fc1e9a8cd4a6663a5686b2025b
b3nj4m1n <b3nj4m1n@gmx.net>
2022-01-24 22:43:09 +0100

    More compatibility; add prompt to start recall; improve EraseablePrint

--- thestuff/memo_trainer/memo.py
+++ thestuff/memo_trainer/memo.py
@@ -26,7 +26,8 @@ class Mode(ABC):
         self.current = self.rnd.random()
         self.time_memo_started = datetime.now()
         self.time_memo_finished = None
-        self.time_answer_checked = None
+        self.time_recall_started = None
+        self.time_recall_finished = None
     def new(self):
         self.i += 1
         self.current = self.rnd.random()
@@ -40,17 +41,21 @@ class Mode(ABC):
         pass
     @abstractmethod
     def check_answer(self, answer):
-        self.time_answer_checked = datetime.now()
+        self.time_recall_finished = datetime.now()
         pass
     def finish_memo(self):
         self.time_memo_finished = datetime.now()
+    def start_recall(self):
+        self.time_recall_started = datetime.now()
+    def finish_recall(self):
+        self.time_recall_finished = datetime.now()
     def stats(self):
         pt = PrettyTable()
         pt.field_names = ["Time Memo", "Time Recall", "Time Total"]
         pt.add_row([
             humanize.precisedelta(self.time_memo_finished - self.time_memo_started, minimum_unit="seconds"),
-            humanize.precisedelta(self.time_answer_checked - self.time_memo_finished, minimum_unit="seconds"),
-            humanize.precisedelta(self.time_answer_checked - self.time_memo_started, minimum_unit="seconds")
+            humanize.precisedelta(self.time_recall_finished - self.time_recall_started, minimum_unit="seconds"),
+            humanize.precisedelta(self.time_recall_finished - self.time_memo_started, minimum_unit="seconds")
         ])
         return pt.get_string()
 
@@ -103,23 +108,49 @@ class ErasablePrint():
     def erase(self):
         columns, lines = shutil.get_terminal_size()
         length = len(self.text)
-        for _ in range(ceil(length/columns)):
+        for _ in range(ceil(length/columns) + self.text.count("\n")):
             print("\r\033[F", end='')
-            print("" * columns, end='')
+            print(" " * columns, end='')
+            print("\r", end='')
+
+# https://stackoverflow.com/a/1394994/11110290
+def wait_for_any_key():
+    try:
+        # Win32
+        from msvcrt import getch
+    except ImportError:
+        # UNIX
+        def getch():
+            import sys, tty, termios
+            fd = sys.stdin.fileno()
+            old = termios.tcgetattr(fd)
+            try:
+                tty.setraw(fd)
+                return sys.stdin.read(1)
+            finally:
+                termios.tcsetattr(fd, termios.TCSADRAIN, old)
+    getch()
+
 
 ep = ErasablePrint()
 while True:
     print("\n" * 2)
-    print("Press any key to start memo.\n")
-    os.system('read -s -n 1')
+    ep.print("Press any key to start memo.\n")
+    wait_for_any_key()
+    ep.erase()
     generator.new()
     ep.print(generator.get_prompt())
-    os.system('read -s -n 1')
+    wait_for_any_key()
     ep.erase()
     generator.finish_memo()
+    ep.print("Press any key to start recall.\n")
+    wait_for_any_key()
+    ep.erase()
+    generator.start_recall()
     prompt = "Enter your answer"
     space = ''.join([" " for _ in  range(len(generator.get_answer()) - len("Enter your answer:"))])
     answer = input(f"{prompt}:{space}\n> ")
+    generator.finish_recall()
     if generator.check_answer(answer):
         print("Correct")
     else:
