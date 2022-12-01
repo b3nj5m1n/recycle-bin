be9a6366ba43399609c1ae8b24218183ed5e350e
b3nj4m1n <b3nj4m1n@gmx.net>
2022-12-01 19:57:47 +0100

    Chapter 7

--- thestuff/byol/src/blip.c
+++ thestuff/byol/src/blip.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,6 +9,64 @@
 
 #include "../vendor/chalk/chalk.h"
 #include "../vendor/mpc/mpc.h"
+
+long eval_op(long x, char *op, long y) {
+  if (strcmp(op, "+") == 0 || strcmp(op, "add") == 0) {
+    return x + y;
+  }
+  if (strcmp(op, "-") == 0 || strcmp(op, "sub") == 0) {
+    return x - y;
+  }
+  if (strcmp(op, "*") == 0 || strcmp(op, "mul") == 0) {
+    return x * y;
+  }
+  if (strcmp(op, "/") == 0 || strcmp(op, "div") == 0) {
+    return x / y;
+  }
+  if (strcmp(op, "%") == 0 || strcmp(op, "mod") == 0) {
+    return x % y;
+  }
+  if (strcmp(op, "^") == 0 || strcmp(op, "pow") == 0) {
+    return pow(x, y);
+  }
+  if (strcmp(op, "min") == 0) {
+    if (x < y) {
+      return x;
+    } else {
+      return y;
+    }
+  }
+  if (strcmp(op, "max") == 0) {
+    if (x > y) {
+      return x;
+    } else {
+      return y;
+    }
+  }
+  return 0;
+}
+
+long eval(mpc_ast_t *t) {
+  // If it's a number, return the number
+  if (strstr(t->tag, "integer")) {
+    return atoi(t->contents);
+  }
+
+  // Operator is second child since first child is '('
+  char *op = t->children[1]->contents;
+
+  // Eval expression after operator
+  long x = eval(t->children[2]);
+
+  // Eval remaining expressions & reduce
+  int i = 3;
+  while (strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+
+  return x;
+}
 
 int main(int argc, char *argv[]) {
 
@@ -23,9 +82,11 @@ int main(int argc, char *argv[]) {
       integer   : /-?[0-9]+/ ;                             \
       operator  : '+' | '-' | '*' | '/' \
                  | \"add\" | \"sub\" | \"mul\" | \"div\" \
-                 | '%' \
+                 | '%' | '^' \
+                 | \"mod\" | \"pow\" \
+                 | \"min\" | \"max\" \
                  ; \
-      expr     : <integer> | <float> | '(' <operator> <expr>+ ')' ;  \
+      expr     : <integer> | '(' <operator> <expr>+ ')' ;  \
       blip     : /^/ <operator> <expr>+ /$/ ;             \
     ",
             Integer, Operator, Expr, Blip);
@@ -42,7 +103,8 @@ int main(int argc, char *argv[]) {
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Blip, &r)) {
       /* On success print and delete the AST */
-      mpc_ast_print(r.output);
+      long result = eval(r.output);
+      printf("%li\n", result);
       mpc_ast_delete(r.output);
     } else {
       /* Otherwise print and delete the Error */
